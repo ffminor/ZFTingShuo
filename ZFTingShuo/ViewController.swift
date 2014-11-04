08812d2ee1cd0898f465c6ea81be47ce755ea3cb
fminor <wangshengli@qingtingfm.com>
2014-11-04 14:20:19 +0800

    Initial Commit

---
+++ ZFTingShuo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZFTingShuo
+//
+//  Created by fminor on 11/4/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
